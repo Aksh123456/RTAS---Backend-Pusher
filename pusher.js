f1bb07a0d31f7f3250b24838dbe4e8a0fa1ed31f
Aksh123456 <aksh.211999@gmail.com>
2024-12-01 20:21:12 +0530

    'add'

--- pusher.js
+++ pusher.js
@@ -1,19 +1,17 @@
 const Pusher = require("pusher");
 require('dotenv').config();
 const pusher = new Pusher({
-  //   appId: "YOUR_APP_ID",
-  //   key: "YOUR_APP_KEY",
-  //   secret: "YOUR_APP_SECRET",
-  //   cluster: "YOUR_APP_CLUSTER",
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
+
+  // appId: process.env.PUSHER_APP_ID,
+  // key: process.env.PUSHER_KEY,
+  // secret: process.env.PUSHER_SECRET,
+  // cluster: process.env.PUSHER_CLUSTER,
+  app_id : "1904164",
+key : "0876dcda9524e34aab76",
+secret :"93ee5f7950ef6d8a9435",
+cluster : "ap2",
   useTLS: true,
 });
 
-// app_id = "1904164"
-// key = "0876dcda9524e34aab76"
-// secret = "93ee5f7950ef6d8a9435"
-// cluster = "ap2"
+
 module.exports = pusher;
