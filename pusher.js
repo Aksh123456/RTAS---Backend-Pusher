f6bc2e97e47975847f7058fb74c8b5b07eb16e1e
Aksh123456 <aksh.211999@gmail.com>
2024-12-02 12:08:33 +0530

    'add'

--- pusher.js
+++ pusher.js
@@ -6,7 +6,7 @@ const pusher = new Pusher({
   // key: process.env.PUSHER_KEY,
   // secret: process.env.PUSHER_SECRET,
   // cluster: process.env.PUSHER_CLUSTER,
-  app_id : "1904164",
+  appId : "1904164",
 key : "0876dcda9524e34aab76",
 secret :"93ee5f7950ef6d8a9435",
 cluster : "ap2",
